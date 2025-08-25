a7bc3263dbaedfaad32bb23f1ca5f4bd52f5c988
ldoubil <baikaiwen12@outlook.com>
2025-08-25 11:48:33 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:aml/core/window_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:aml/src/rust/frb_generated.dart';
 
+import 'storage/debug_commands.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await RustLib.init();
